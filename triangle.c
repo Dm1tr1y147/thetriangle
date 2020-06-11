f9020d0b9b6242f5a61294d2b12d49fdde0b4e76
Dmitriy Shishkov <me@dmitriy.icu>
2020-06-11 12:12:18 +0500

    Working on new user interphase

--- triangle.c
+++ triangle.c
@@ -1,26 +1,72 @@
 #include <stdio.h>
+#include <string.h>
 
+void clrscr();
+void drawTriangle(char *type, int height, char symbol1, char symbol2);
 void drawLine(int freeSpace, int length, char symbol);
 
 int main()
 {
-    int height;
-    scanf("%d\n", &height);
-    char symbol1, symbol2;
-    scanf("%c %c", &symbol1, &symbol2);
+    while (1)
+    {
+        clrscr();
+        printf("\n\n");
+        printf("  *   Welcom to The Triangle programm.\n");
+        printf(" ***  It would help you to draw some simple, but beautiful triangles.\n");
+        printf("***** Hope you'll enjoy using it!\n");
+        printf("\n\n");
+
+        sleep(2);
+
+        clrscr();
+        printf("\n\n");
+        printf("Select what mode do you want to work with:\n");
+
+        drawTriangle("so", 5, '*', '0');
+        printf("(so)lid\n");
+
+        drawTriangle("st", 5, '*', '+');
+        printf("(st)ripped\n");
+
+        char type[2];
+        fgets(type, 2, stdin);
+    }
+
+    return 0;
+}
+
+void clrscr()
+{
+    printf("\e[1;1H\e[2J");
+}
+
+void drawTriangle(char *type, int height, char symbol1, char symbol2)
+{
+    if (!(strncmp(type, "so", 2)))
+    {
+        symbol2 = symbol1;
+    }
+    else if (!(strncmp(type, "st", 2)))
+    {
+    }
+    else
+    {
+        printf("Wrong input, try angain");
+    }
 
     for (int i = 0; i < height; i++)
     {
         char curSymbol;
-        if ((i + 1) % 2 == 1) {
+        if ((i + 1) % 2 == 1)
+        {
             curSymbol = symbol1;
-        } else {
+        }
+        else
+        {
             curSymbol = symbol2;
         }
         drawLine(height - i - 1, i * 2 + 1, curSymbol);
     }
-
-    return 0;
 }
 
 void drawLine(int freeSpace, int length, char symbol)
